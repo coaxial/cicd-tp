5dd43b3334c10a5743ce2e2b9669377bc81cf969
Tanguy RAGUENES <tanguy.raguenes2024@campus-eni.fr>
2025-12-08 14:02:02 +0100

    test workflow

--- tests/unit/greeting.test.js
+++ tests/unit/greeting.test.js
@@ -4,4 +4,8 @@ describe("getGreeting", () => {
   it("returns the hello world message", () => {
     expect(getGreeting()).toBe("Hello world!");
   });
+
+  it("fails on purpose to test CI", () => {
+    expect(1 + 1).toBe(3);
+  });
 });
